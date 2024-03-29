6d463686bb3768b49e169db5eddfae8239ebe3ff
atilboztepe <atilboztepe@gmail.com>
2024-03-28 22:14:37 -0400

    all classes completed

--- src/Week1/divisionWithoutOperator.java
+++ src/Week1/divisionWithoutOperator.java
@@ -4,10 +4,21 @@ public class divisionWithoutOperator {
     public static void main(String[] args) {
         //Write a method that can divide two numbers without using division operator
 
-
+        int dividend = 15;
+        int divisor = 3;
+        System.out.println(divide(dividend, divisor)); // Output: 5
     }
 
 
+    public static int divide(int dividend, int divisor) {
+        int quotient = 0;
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
 
+        while (dividend >= divisor) {
+            dividend -= divisor;
+            quotient++;
+        }
 
+        return quotient * sign;
+    }
 }
--- src/Week1/finra.java
+++ src/Week1/finra.java
@@ -6,9 +6,20 @@ public class finra {
         //print "FIN" instead of the number and
         //for numbers which are a multiple of 5, print "RA" instead of the number.
         //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
-
+        finra();
 
     }
 
+    public static void finra() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else System.out.println(i);
+        }
+    }
 
 }
